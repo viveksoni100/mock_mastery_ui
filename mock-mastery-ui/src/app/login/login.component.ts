9e1175291a61e7834e8d7569b975a5a9d6777491
viveksoni100 <viveksoni100@gmail.com>
2024-03-16 15:00:28 +0530

    login component added

--- mock-mastery-ui/src/app/login/login.component.ts
+++ mock-mastery-ui/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   focus!: boolean;
   focus1!: boolean;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  callOauthGoogleAPI() {
+    console.log('clicked!');
+    // this.router.navigateByUrl('http://localhost:9090/mock-mastery/OAuth/signInOAuthGoogle');
+    // window.location.href = 'http://localhost:9090/mock-mastery/OAuth/signInOAuthGoogle';
+    window.location.href = 'http://localhost:9090/mock-mastery/login';
+  }
 }
